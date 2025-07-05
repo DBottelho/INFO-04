aad329dc8ae26b0fa2a38a1364149d4836100a1a
DBottelho <bottelho.ti@gmail.com>
2025-07-05 09:11:07 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,11 @@
+console.log("Contador usando FOR:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("\nContador usando WHILE:");
+let j = 1;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
